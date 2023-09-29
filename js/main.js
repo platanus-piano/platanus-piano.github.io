e2217278d1559e0c3371e73c491de509ffcda8b0
kai0930 <gamo.kai.1111@gmail.com>
2023-09-30 01:04:59 +0900

    新HPを反映

--- js/main.js
+++ js/main.js
@@ -1,63 +1,140 @@
-var getUrlParameter = function(sParam) {
-    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
-
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
-
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : sParameterName[1];
-        }
-    }
-};
-
-var program2html = function(program, flag = false) {
-    var str = "";
-    var prev_year = "0000";
-    var prev_id = -1;
-    for(var playing of program) {
-        if(playing.year != prev_year && flag){
-            str += "<h3>" + playing.year + "</h3>";
-            prev_year = playing.year;
-        }
-        if(playing.id != prev_id && flag){
-            str += "<h4><a href='/archive/concert/?id=" + playing.id + "'>";
-            str += playing.concert_name;
-            //str += " " + playing.year;
-            str += "</a></h4>";
-            prev_id = playing.id;
-        }
-        str += "<ul>";
-        str += "<li><p>";
-        str += "<strong>" + playing.name + "</strong>";
-        str += "<br>";
-        str += "<a href='/archive/composer/?name=" + playing.composer + "'>";
-        str += playing.composer;
-        str += "</a>";
-
-        if(playing.arrangedby){
-            str += " (" + playing.arrangedby + " 編曲)";
-        }
-        str += "</p></li>";
-
-        str += "<ul>";
-            for(var player of playing.players){
-                str += "<li><p>";
-
-                str += "<tt>" + player.instrument + "</tt>";
-                str += " : ";
-
-                str += "<a href='/archive/player/?name=" + player.name + "'>";
-                str += player.name;
-                str += "</a>";
+;(function () {
+	
+	'use strict';
 
 
-                str += "</p></li>";
-            }
-        str += "</ul>";
-        str += "</ul>";
-    }
-    return str;
-}
+
+	// iPad and iPod detection	
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	// Main Menu Superfish
+	var mainMenu = function() {
+
+		$('#fh5co-primary-menu').superfish({
+			delay: 0,
+			animation: {
+				opacity: 'show'
+			},
+			speed: 'fast',
+			cssArrows: true,
+			disableHI: true
+		});
+
+	};
+
+	// Parallax
+	var parallax = function() {
+		$(window).stellar();
+	};
+
+
+	// Offcanvas and cloning of the main menu
+	var offcanvas = function() {
+
+		var $clone = $('#fh5co-menu-wrap').clone();
+		$clone.attr({
+			'id' : 'offcanvas-menu'
+		});
+		$clone.find('> ul').attr({
+			'class' : '',
+			'id' : ''
+		});
+
+		$('#fh5co-page').prepend($clone);
+
+		// click the burger
+		$('.js-fh5co-nav-toggle').on('click', function(){
+
+			if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			} else {
+				$('body').addClass('fh5co-offcanvas');
+			}
+			// $('body').toggleClass('fh5co-offcanvas');
+
+		});
+
+		$('#offcanvas-menu').css('height', $(window).height());
+
+		$(window).resize(function(){
+			var w = $(window);
+
+
+			$('#offcanvas-menu').css('height', w.height());
+
+			if ( w.width() > 769 ) {
+				if ( $('body').hasClass('fh5co-offcanvas') ) {
+					$('body').removeClass('fh5co-offcanvas');
+				}
+			}
+
+		});	
+
+	}
+
+	
+
+	// Click outside of the Mobile Menu
+	var mobileMenuOutsideClick = function() {
+		$(document).click(function (e) {
+	    var container = $("#offcanvas-menu, .js-fh5co-nav-toggle");
+	    if (!container.is(e.target) && container.has(e.target).length === 0) {
+	      if ( $('body').hasClass('fh5co-offcanvas') ) {
+				$('body').removeClass('fh5co-offcanvas');
+			}
+	    }
+		});
+	};
+
+
+	// Animations
+
+	var contentWayPoint = function() {
+		var i = 0;
+		$('.animate-box').waypoint( function( direction ) {
+
+			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
+				
+				i++;
+
+				$(this.element).addClass('item-animate');
+				setTimeout(function(){
+
+					$('body .animate-box.item-animate').each(function(k){
+						var el = $(this);
+						setTimeout( function () {
+							el.addClass('fadeInUp animated');
+							el.removeClass('item-animate');
+						},  k * 200, 'easeInOutExpo' );
+					});
+					
+				}, 100);
+				
+			}
+
+		} , { offset: '85%' } );
+	};
+	
+
+	// Document on load.
+	$(function(){
+		mainMenu();
+		parallax();
+		offcanvas();
+		mobileMenuOutsideClick();
+		contentWayPoint();
+		
+
+	});
+
+
+}());
