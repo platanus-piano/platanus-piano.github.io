95c03fad746b7d4274f07d92288466966833850a
kai0930 <gamo.kai.1111@gmail.com>
2023-10-13 15:52:28 +0900

    fix ga

---
+++ src/configs/googleAnalytics/gtag.ts
@@ -0,0 +1,28 @@
+export const GOOGLE_ANALYTICS_ID =
+  String(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID) || '';
+
+export const pageview = (url: string) => {
+  if (!GOOGLE_ANALYTICS_ID) return;
+  window.gtag('config', GOOGLE_ANALYTICS_ID, {
+    page_path: url,
+  });
+};
+
+export const event = ({
+  action,
+  category,
+  label,
+  value,
+}: {
+  action: string;
+  category: string;
+  label: string;
+  value?: number;
+}): void => {
+  if (!GOOGLE_ANALYTICS_ID) return;
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ import theme from '@/configs/mui/theme';
 import createEmotionCache from '@/configs/mui/createEmotionCache';
 import Layout from '@/components/layout/Layout';
 import Script from 'next/script';
+import { usePageView } from '@/hooks/GAPageView';
+import { GOOGLE_ANALYTICS_ID } from '@/configs/googleAnalytics/gtag';
 
 const clientSideEmotionCache = createEmotionCache();
 interface MyAppProps extends AppProps {
@@ -15,25 +17,32 @@ interface MyAppProps extends AppProps {
 
 function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  usePageView();
+
   return (
     <CacheProvider value={emotionCache}>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
+      {GOOGLE_ANALYTICS_ID && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
+          />
+          <Script
+            id="gtag-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
            window.dataLayer = window.dataLayer || [];
            function gtag(){dataLayer.push(arguments);}
            gtag('js', new Date());
  
-           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}');
+           gtag('config', '${GOOGLE_ANALYTICS_ID}');
            `,
-        }}
-      />
+            }}
+          />
+        </>
+      )}
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
