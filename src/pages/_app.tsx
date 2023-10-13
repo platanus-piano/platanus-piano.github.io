2fca551d42b6e2013c6e9a2e54735049e6e43b1a
kai0930 <gamo.kai.1111@gmail.com>
2023-10-13 15:05:31 +0900

    Update _app.tsx
    add gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import { CacheProvider, EmotionCache } from '@emotion/react';
 import theme from '@/configs/mui/theme';
 import createEmotionCache from '@/configs/mui/createEmotionCache';
 import Layout from '@/components/layout/Layout';
+import Script from 'next/script';
 
 const clientSideEmotionCache = createEmotionCache();
 interface MyAppProps extends AppProps {
@@ -16,6 +17,23 @@ function MyApp(props: MyAppProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
   return (
     <CacheProvider value={emotionCache}>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+ 
+           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_TRACKING_ID}');
+           `,
+        }}
+      />
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
